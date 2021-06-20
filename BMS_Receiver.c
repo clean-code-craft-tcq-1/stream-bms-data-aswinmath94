328dec8c564d6c54f8a4a82d45d1a714956f8763
Padmashree-DJ <77479478+Padmashree-DJ@users.noreply.github.com>
2021-06-20 15:13:52 +0530

    Create BMS_Receiver.c

---
+++ BMS_Receiver.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+void readfromConsole(void)
+{
+	char buffer[200];
+	while (scanf("%[^\n]s",buffer)!=EOF)
+	{
+		printf(" the value of the buffer is %s\n",buffer);
+	}
+
+}
+
+int main()
+{
+	readfromConsole();
+	return 0;
+}
