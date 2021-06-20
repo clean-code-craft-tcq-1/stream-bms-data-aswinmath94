a85c0990d4fabe2262f0d1ca611e8f984c6adbc4
ECHO is on <@apac.bosch.com>
2021-06-20 22:15:42 +0530

    added .c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -10,14 +10,41 @@
 void readfromConsole(void)
 {
 	char input[1024];
+	int i,j=0;
+	int paramindex=0
 	const char * InputArray[] = {
     "{'temperature':",
     "'soc':"
 };
 
-   while (scanf("%s\n", input) !=EOF) {
-	    
-        printf("the value of the input is %s\n", input); 
+   while (scanf("%s\n", input) !=EOF) 
+   {
+	   if ( (intput== InputArray[0])|| (intput== InputArray[1]))
+	   {
+		   printf("the value of the character is %s\n", input);
+	    continue;
+	   }
+	   else
+	   {
+		   
+			   i = strlen(valuefromsender);
+               InputArray[i] = '\n';
+               InputArray[i + 1] = '\0';
+		   if(paramindex%1!=0)
+		   {
+			   Temperature[j]= atof(InputArray);
+			   printf("the value of the temp is %s\n", input);
+			   paramindex++;
+		   }
+		   else 
+		   {
+			   SOC[j]=atof(InputArray);
+			   printf("the value of the SOC is %s\n", input);
+			   paramindex=0;
+		   }
+		   j++;
+	   }
+          
    }
 	
 
