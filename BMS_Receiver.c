94dd5ee54577f0adcb8ac0432f38cc44eed50ad5
Padmashree-DJ <77479478+Padmashree-DJ@users.noreply.github.com>
2021-06-20 21:14:14 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -12,7 +12,7 @@ void readfromConsole(void)
 	char input[1024];
     char* s;
 
-   while (1 == scanf("%[^\n]%*c", input)) {
+   while (1 == scanf("%[^\n]%s", input)) {
         printf("the value of the input is %s\n", input); /* The input was not a blank line */
    }
 	
