cbdf653c570f4705547dbd2b4f234a5692300027
Padmashree-DJ <77479478+Padmashree-DJ@users.noreply.github.com>
2021-06-20 20:32:55 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -8,11 +8,21 @@
 
 void readfromConsole(void)
 {
-	char buffer[200];
-	while (scanf("%[^\n]s",buffer)!=EOF)
-	{
-		printf(" the value of the buffer is %s\n",buffer);
-	}
+	char input[1024];
+    char* s;
+
+   while (fgets(input, 1024, stdin) != NULL) 
+   {
+   
+     /* Here we suppose the fgets() has reached a '\n' character... */
+     for (char* s = input; (*s != '\n') && isspace(*s); s++)
+        ; /* skipping blanks */
+     if (*s == '\n')
+        break; /* Blank line */
+     else
+        printf("the value of the input is %s\n", input); /* The input was not a blank line */
+   }
+	
 
 }
 
